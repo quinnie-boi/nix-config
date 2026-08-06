e736d6be486c36cc85963d2edf21b3d3ad4554e0
AbrasiveAlmond <qpearson.nz@gmail.com>
2026-08-06 20:08:14 +1200

    finish up devshell stuff

--- devshells/default.nix
+++ devshells/default.nix
@@ -5,4 +5,6 @@
   python = import ./python.nix { inherit pkgs; };
   typst = import ./typst.nix { inherit pkgs; };
   uv = import ./uv.nix { inherit pkgs; };
+  r = import ./r.nix { inherit pkgs; };
+  latex = import ./latex.nix { inherit pkgs; };
 }
--- devshells/python.nix
+++ devshells/python.nix
@@ -5,9 +5,12 @@ pkgs.mkShell {
     (pkgs.python3.withPackages (python-pkgs: [
       python-pkgs.numpy
       python-pkgs.matplotlib
-      python-pkgs.ruff
       python-pkgs.sympy
+      python-pkgs.pandas
+      python-pkgs.ruff
       python-pkgs.scipy
+      python-pkgs.tree-sitter-python
+      python-pkgs.debugpy
     ]))
     pkgs.ty
   ];
